a64324b3d7e6a86c1a615c589378731036afdc04
trenta3 <dario.balboni.96@gmail.com>
2016-10-04 15:02:23 +0200

    Aggiunto script modificato .travis.yml

---
+++ .upload-file.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+cd ../compiled-pdfs/;
+curl --header "Content-Type: multipart/form-data" -F file=@$(ls -1 *.pdf | head -n 1 | tr -d '\n' ) https://uz.sns.it/~trenta3/Zannier1617/upload.php?key=${BALBO_KEY}
+
+
