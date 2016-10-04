cc75f9710dd8332cc55e669119f6fa7bba68e2d2
trenta3 <dario.balboni.96@gmail.com>
2016-10-04 20:17:04 +0200

    Messo a posto script di upload dei file

--- .upload-file.sh
+++ .upload-file.sh
@@ -5,6 +5,8 @@ export CURRENT_BRANCH=$(if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then echo $TRA
 if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
     cd /home/travis;
     curl --header "Content-Type: multipart/form-data" -F 'file=@lezioni.pdf;filename=Zannier1617-${CURRENT_BRANCH}.pdf' https://uz.sns.it/~trenta3/Zannier1617/upload.php?key=${BALBO_KEY}
+else
+    echo "Siamo in una PR, non aggiorno"
 fi
 
 cat /home/travis/build.sh;
