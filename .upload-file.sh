97643af48b3694633361dd480b434780393a2297
trenta3 <dario.balboni.96@gmail.com>
2016-10-04 20:47:40 +0200

    Aggiornato upload-file References PR #20

--- .upload-file.sh
+++ .upload-file.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export CURRENT_BRANCH=$(if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then echo $TRAVIS_BRANCH; else echo $TRAVIS_PULL_REQUEST_BRANCH; fi)
+CURRENT_BRANCH=$(if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then echo $TRAVIS_BRANCH; else echo $TRAVIS_PULL_REQUEST_BRANCH; fi)
 
 if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
     curl --header "Content-Type: multipart/form-data" -F 'file=@lezioni.pdf;filename=Zannier1617-${CURRENT_BRANCH}.pdf' https://uz.sns.it/~trenta3/Zannier1617/upload.php?key=${BALBO_KEY}
