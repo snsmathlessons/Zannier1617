5495a427c41b9e98df2efc1301ee3e79cd8071e5
trenta3 <dario.balboni.96@gmail.com>
2016-10-04 15:39:22 +0200

    Boh

--- .upload-file.sh
+++ .upload-file.sh
@@ -3,7 +3,9 @@
 export CURRENT_BRANCH=$(if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then echo $TRAVIS_BRANCH; else echo $TRAVIS_PULL_REQUEST_BRANCH; fi)
 
 if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
+    cd /home/travis;
     curl --header "Content-Type: multipart/form-data" -F 'file=@lezioni.pdf;filename=Zannier1617-${CURRENT_BRANCH}.pdf' https://uz.sns.it/~trenta3/Zannier1617/upload.php?key=${BALBO_KEY}
 fi
 
+cat /home/travis/build.sh;
 
