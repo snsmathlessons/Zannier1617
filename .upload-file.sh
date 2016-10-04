994026a25ccd66ae51aac9cb0607ba1c003782e7
trenta3 <dario.balboni.96@gmail.com>
2016-10-04 22:59:29 +0200

    Aggiornato upload-file.sh

--- .upload-file.sh
+++ .upload-file.sh
@@ -3,7 +3,7 @@
 CURRENT_BRANCH=$(if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then echo $TRAVIS_BRANCH; else echo $TRAVIS_PULL_REQUEST_BRANCH; fi)
 
 if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
-    curl --header "Content-Type: multipart/form-data" -F 'file=@lezioni.pdf;filename=Zannier1617-${CURRENT_BRANCH}.pdf' https://uz.sns.it/~trenta3/Zannier1617/upload.php?key=${BALBO_KEY}
+    curl --header "Content-Type: multipart/form-data" -F "file=@lezioni.pdf;filename=Zannier1617-${CURRENT_BRANCH}.pdf" https://uz.sns.it/~trenta3/Zannier1617/upload.php?key=${BALBO_KEY}
 else
     echo "Siamo in una PR, non aggiorno"
 fi
